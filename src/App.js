8a154d7a65f9567d293defd8d8d332681e9f584d
jbpixel52 <50649423+jbpixel52@users.noreply.github.com>
2022-11-22 14:49:06 -0800

    ready for deployment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
+import reportWebVitals from "./reportWebVitals";
+
 import React, { useEffect, useState } from "react";
 import { createWorkerFactory, useWorker } from "@shopify/react-web-worker";
 const createWorker = createWorkerFactory(() => import("./worker"));
@@ -9,38 +11,54 @@ function App() {
   const worker2 = useWorker(createWorker);
   const [message1, setMessage1] = React.useState(null);
   const [message2, setMessage2] = React.useState(null);
-  let date = new Date().getTime();
-  console.log(`start time: ${date}`);
+  const [clicked, setClicked] = React.useState(false);
+  let startTime = new Date().getTime();
+  //console.log(`start time: ${startTime}`);
   useEffect(() => {
-    console.log(`message 1 is ${message1}`);
-    console.log(`message 2 is ${message2}`);
+    //console.log(`message 1 is ${message1}`);
+    //console.log(`message 2 is ${message2}`);
     (async () => {
       // Note: in your actual app code, make sure to check if Home
       // is still mounted before setting state asynchronously!
 
       const webWorkerMessage1 = await worker1
-        .Generator(1000, 1)
+        .Generator(50, 1)
         .then((response) => {
           setMessage1(response);
+          console.log(
+            `message 1 duration: ${new Date().getTime() - startTime}`
+          );
         });
       const webWorkerMessage2 = await worker2
-        .Generator(1000, 2)
+        .Generator(5, 2)
         .then((response) => {
           setMessage2(response);
         });
     })();
-  }, [worker1, worker2]);
+  }, [clicked]);
 
   return (
-    <div className="w-screen h-auto flex flex-col bg-cyan-900">
-      <h1 className="text-white">WEB WORKERS 🤖</h1>
-      <br />
-      <p className="font-bold">{message1}</p>
-      <p className="flex max-w-1/2 text-red-500 flex-wrap break-all	">
-        {message2}
-      </p>
+    <div className="flex h-screen flex-col max-w-screen bg-gradient-to-tl from-indigo-200 via-red-200 to-yellow-100`">
+      <h1 className="w-fit m-auto text-black font-bold text-5xl p-10">
+        WEB WORKERS 🤖
+      </h1>
+      <button
+        className="block m-auto w-fit h-fit text-xl rounded-full p-5 btn font-bold shadow-2xl hover:bg-red-300"
+        onClick={() => setClicked(!clicked)}
+      >
+        CLICK ME TO INVENT WORDS!
+      </button>
+      <div className="m-auto flex flex-row gap-10 p-10">
+        <div className="flex flex-wrap max-w-1/2 gap-3 rounded p-2 shadow-xl bg-amber-300">
+          {message1}
+        </div>
+        <div className="flex flex-wrap max-w-1/2 gap-3 rounded p-2 shadow-xl bg-emerald-300">
+          {message2}
+        </div>
+      </div>
     </div>
   );
 }
+//reportWebVitals(console.log);
 
 export default App;
--- src/worker.js
+++ src/worker.js
@@ -1,29 +1,33 @@
-import { worker } from "jshint/src/vars";
 import json from "./index.json";
+import reportWebVitals from "./reportWebVitals";
+
 export function Generator(iterations, id) {
   let workerStartDate = new Date().getTime();
   let parsed = Object.values(json);
   let generated = [];
   //RULES...
   //  prefix + base + sufix
-  //
-
-  let maxLength = 5;
-  for (let index = 0; index < iterations; index++) {
+  //  
+  let baseLength = 7;
+  let maxLength = baseLength;
+  while (generated.length < iterations) {
     var prefix = parsed[Math.floor(Math.random() * parsed.length)];
+    prefix = prefix.slice(0, prefix.length / 2);
     var base = parsed[Math.floor(Math.random() * parsed.length)];
+    base = base.slice(base.length / 3, (base.length / 3) * 2);
     var sufix = parsed[Math.floor(Math.random() * parsed.length)];
+    base = base.slice((base.length / 3) * 2, base.length);
+
     var tempWord = prefix + base + sufix;
-    if (tempWord.length < maxLength) {
-      generated.push(`${tempWord}`);
-    } else {
-      maxLength = maxLength + 1;
+    if (tempWord.length > maxLength) {
+      //console.log(`${tempWord} too long...`);
+      maxLength = baseLength + 1;
+    } else if (tempWord.length <= maxLength) {
+      generated.push(<p>{tempWord}</p>);
+      maxLength = baseLength;
     }
   }
+  //reportWebVitals(console.log);
 
-  console.log(
-    `duration of generator${id}: ${new Date().getTime() - workerStartDate}`
-  );
-  console.log(generated);
   return generated;
 }
