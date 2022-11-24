3dddb15585308515ea644f8dd22fd68a447fb4a9
jbpixel <50649423+jbpixel52@users.noreply.github.com>
2022-11-23 16:13:51 -0800

    gg

--- src/worker.js
+++ src/worker.js
@@ -1,28 +1,24 @@
 import json from "./index.json";
-import reportWebVitals from "./reportWebVitals";
-
 export function Generator(iterations, id) {
-  let workerStartDate = new Date().getTime();
   let parsed = Object.values(json);
   let generated = [];
-  //RULES...
-  //  prefix + base + sufix
-  //  
+  const randomParsed = () => Math.floor(Math.random() * parsed.length);
+  //RULES. =>   prefix + base + sufix
   let baseLength = 7;
   let maxLength = baseLength;
   while (generated.length < iterations) {
-    var prefix = parsed[Math.floor(Math.random() * parsed.length)];
+    var prefix = parsed[randomParsed()];
     prefix = prefix.slice(0, prefix.length / 2);
-    var base = parsed[Math.floor(Math.random() * parsed.length)];
+    var base = parsed[randomParsed()];
     base = base.slice(base.length / 3, (base.length / 3) * 2);
-    var sufix = parsed[Math.floor(Math.random() * parsed.length)];
+    var sufix = parsed[randomParsed()];
     base = base.slice((base.length / 3) * 2, base.length);
-
     var tempWord = prefix + base + sufix;
     if (tempWord.length > maxLength) {
       //console.log(`${tempWord} too long...`);
       maxLength = baseLength + 1;
     } else if (tempWord.length <= maxLength) {
+      //push <p> </p> HTML into generated array of words;
       generated.push(<p>{tempWord}</p>);
       maxLength = baseLength;
     }
